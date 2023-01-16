10f74c53ad0337b44ea25043f1b9096963fb3afb
act731 <bjoern.leuendorf@haw-hamburg.de>
2023-01-16 10:38:00 +0100

    turning all lights off in the beginning

--- src/fsm/Connecting.cpp
+++ src/fsm/Connecting.cpp
@@ -12,6 +12,9 @@ Connecting::~Connecting() {}
 
 void Connecting::entry() {
 	std::cout << "Connecting entry" << std::endl;
+	for (int i = 1; i < 8; i++) {
+		action->lightOff(i);
+	}
 	establishConnection();
 	if (MsgSendPulse(coid, -1, static_cast<int>(SET_SW_TYPE), 0) == -1) {
 			perror("MsgSendPulse failed");
