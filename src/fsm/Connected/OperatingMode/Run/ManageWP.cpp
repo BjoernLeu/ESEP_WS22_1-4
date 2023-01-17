31d26478413f5304a47b3e60e173f5e28acff81e
Pascal Kießler <pascal.kiessler@haw-hamburg.de>
2023-01-17 09:46:00 +0100

    bernds gelöscht

--- src/fsm/Connected/OperatingMode/Run/ManageWP.cpp
+++ src/fsm/Connected/OperatingMode/Run/ManageWP.cpp
@@ -48,23 +48,19 @@ bool ManageWP::handleLbO()
 
 void ManageWP::startWatchLateThread()
 {
-	std::cout << "Bernd 1" << std::endl;	
 	std::thread wT (&ManageWP::watchWPLate, this);
 	wT.detach();
 }
 
 void ManageWP::watchWPLate() {
-	std::cout << "Bernd 2" << std::endl;
 	bool watching = true;
 	auto prev = std::chrono::system_clock::now();
 	auto now = std::chrono::system_clock::now();
 
-	std::cout << "Bernd 3" << std::endl;
 	while(watching) {
 		//std::cout << "Bernd 4" << std::endl;
 		now = std::chrono::system_clock::now();
 		if(data->motor && !data->motorStop) {
-			std::cout << "Bernd 5" << std::endl;
 			std::chrono::duration<double> elapsed_seconds = now-prev;
 			double addDistance = elapsed_seconds.count();
 			if(data->motorSlow) {
